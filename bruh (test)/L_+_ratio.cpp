09bf97b871edab1a03d0883768f64c5afa0f31ea
codyprupp <crupp@ucsd.edu>
2023-08-12 12:12:46 -0700

    lmao

---
+++ bruh (test)/L_+_ratio.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+
+void uhh_ratio() {
+    printf("\n L L L L L L (tuas) (epic)");
+}
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "bruh (test)/L_+_ratio.cpp"
+
+int main() {
+    std::cout << "hello" << std::endl;
+    printf("difojpdsof");
+
+    uhh_ratio();
+
+    return 0;
+}
