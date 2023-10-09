b745c40c49f4b1984e6e665aacc1b7d36ce725da
HuangMichael94 <mshuang2001@gmail.com>
2023-10-08 22:42:03 -0700

    Constructor that takes in (0,0) point in lat,lon

--- include/pathing/cartesian.hpp
+++ include/pathing/cartesian.hpp
@@ -1,14 +1,31 @@
 #ifndef UTILITIES_CARTESIAN_HPP_
 #define UTILITIES_CARTESIAN_HPP_
+#define _USE_MATH_DEFINES
+#include <map>
+#include <string>
+#include <iostream>
+#include <stdexcept>
 
 class CartesianConverter {
     /*
         TODO:
-
-        Constructor that takes in (0,0) point in lat,lon
-        Function that takes XYZCoord and returns GPSCoord
-        Function that takes GPSCoord and returns XYZCoord 
+        Function that takes GPSCoord and returns XYZCoord
+        Function that takes XYZCoord and returns GPSCoord 
     */
+
+    private:
+        std::map<std::string,float> center;
+
+    public:
+        // Constructor that takes in (center point) in latitude and longitude format.
+        CartesianConverter(std::map<std::string,float> center_point){
+            if((center_point.find("latitude") == center_point.end()) || (center_point.find("longitude") == center_point.end())){
+                throw std::invalid_argument("Center point must have the \"latitude\" and \"longitude\" keys.");
+            }
+
+            center = center_point;
+        };
+
 };
 
 #endif // UTILITIES_CARTESIAN_HPP_
