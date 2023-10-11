6cdd351879dbb2275cf69a11b2075e493387ba56
AchuthKrishna <92132574+AchuthKrishna@users.noreply.github.com>
2023-10-11 15:22:01 -0700

    Add Data Structure Files
    Added header and source file for RRT Tree data structure

---
+++ include/pathing/tree.hpp
@@ -0,0 +1,19 @@
+#ifndef PATHING_TREE_HPP_
+#define PATHING_TREE_HPP_
+
+class RRTPoint {
+    // TODO: Declare methods/fields
+}
+
+class RRTNode {
+    // TODO: Declare methods/fields
+}
+
+class RRTEdge {
+    // TODO: Declare methods/fields
+}
+
+class RRTTree {
+    // TODO: Declare methods/fields
+}
+#endif // PATHING_TREE_HPP_
