ada892fbcacb374102adbe27093c0bf395129855
Anthony Tarbinian <atar137h@gmail.com>
2023-10-11 23:50:41 -0700

    overhaul tests cmake
    place tests cmake setup in tests folder
    
    auto-detect new test files
    
    added a test target to run tests on "make test". running this target
    will recompile the tests if out of date.

--- tests/hello_test.cpp
+++ tests/hello_test.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 
 // Demonstrate some basic assertions.
-TEST(HelloTest, BasicAssertions) {
+TEST(HelloTest, HelloTest1) {
   // Expect two strings not to be equal.
   EXPECT_STRNE("hello", "world");
   // Expect equality.
