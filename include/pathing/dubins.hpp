883dc0903a4c654e74b84c789e2c9fb9ba4fa89a
Christopher Lee <chrisdenglee@gmail.com>
2023-10-16 20:35:12 +0000

    starting dubins transfer

---
+++ include/pathing/dubins.hpp
@@ -0,0 +1,12 @@
+#ifndef PATHING_DUBINS_HPP_
+#define PATHING_DUBINS_HPP_
+
+class Dubins {
+    /*
+        TODO: 
+            - implement a package that implements LINALG
+            - copy the python code from the other obc repo
+    */
+}
+
+#endif // PATHING_DUBINS_HPP_
---
+++ tests/dubins_test.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(HelloTest, HelloTest1) {
+    // TODO: Write Tests
+}
+
