daa11da486fdcd42ffd7f1c391e1b3b4d3d653b9
Samir Rashid <samirsgotmail@gmail.com>
2023-10-22 17:07:19 -0700

    commit late commit infrequently

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 
 #include "core/states.hpp"
 
+// #include <opencv2/opencv.hpp>
+// #include <torch/torch.h>
+#include <iostream>
+
 int main() {
     std::cout << "hello" << std::endl;
 
@@ -9,5 +13,7 @@ int main() {
 
     state.tick();
 
+//   torch::Tensor tensor = torch::rand({2, 3});
+//   std::cout << tensor << std::endl;
     return 0;
 }
