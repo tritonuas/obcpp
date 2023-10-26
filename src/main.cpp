2074a40cd56dd70248fb96a2d7e39cb8855d9498
Tyler Lentz <tlentz@ucsd.edu>
2023-10-26 23:49:42 +0000

    remove test code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include "Eigen"
 #include "core/states.hpp"
 
 int main() {
@@ -9,11 +8,5 @@ int main() {
 
     state.tick();
 
-    Eigen::MatrixXd m(2,2);
-    m(0,0) = 3;
-    m(1,0) = 2.5;
-    m(0,1) = -1;
-    m(1,1) = m(1,0) + m(0,1);
-    std::cout << m << std::endl;
     return 0;
 }
