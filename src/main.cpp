206831c1e6ba33dfcb1f41cf62fbb5f1ad56170e
Samir Rashid <samirsgotmail@gmail.com>
2023-10-25 23:23:46 -0700

    contrib

--- src/main.cpp
+++ src/main.cpp
@@ -19,12 +19,25 @@ int main() {
   std::cout << tensor << std::endl;
 
       //create a gui window:
-    // namedWindow("Output",1);
+    namedWindow("Output",1);
     
     //initialize a 120X350 matrix of black pixels:
     Mat output = Mat::zeros( 120, 350, CV_8UC3 );
-    std::cout << output << std::endl;
     
-   
+    //write text on the matrix:
+    putText(output,
+            "Hello World :)",
+            cvPoint(15,70),
+            FONT_HERSHEY_PLAIN,
+            3,
+            cvScalar(0,255,0),
+            4);
+    
+    //display the image:
+    imshow("Output", output);
+    
+    //wait for the user to press any key:
+    waitKey(0);
+    
     return 0;
 }
