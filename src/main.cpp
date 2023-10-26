f8bc7584afca4bb3a2529a463b3175185bdd52b0
Christopher Lee <chrisdenglee@gmail.com>
2023-10-26 23:42:50 +0000

    add Eigen

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include "Eigen"
 #include "core/states.hpp"
 
 int main() {
@@ -9,5 +9,11 @@ int main() {
 
     state.tick();
 
+    Eigen::MatrixXd m(2,2);
+    m(0,0) = 3;
+    m(1,0) = 2.5;
+    m(0,1) = -1;
+    m(1,1) = m(1,0) + m(0,1);
+    std::cout << m << std::endl;
     return 0;
 }
