0c7256c77a43431ca60123c0cf6143c380d582e8
Christopher Lee <chris@Christophers-MacBook-Pro-5.local>
2023-10-29 15:32:38 -0700

    more dubins things

--- include/pathing/dubins.hpp
+++ include/pathing/dubins.hpp
@@ -1,6 +1,9 @@
 #ifndef PATHING_DUBINS_HPP_
 #define PATHING_DUBINS_HPP_
 
+#include <math.h>
+
+#include "../utilities/datatypes.hpp"
 #include "Eigen" 
 
 struct DubinsPath {
@@ -21,32 +24,65 @@ struct RRTOption {
     bool has_straight;
 };
 
-/*
- * [TODO] write descritpion
+/**
+ *  @param vector 2-vector
+ *  @return orthogonal 2-vector to @param vector (always a 90 degree rotation counter-clockwise)
+ *  @see https://mathworld.wolfram.com/PerpendicularVector.html
  */
 Eigen::Vector2d findOrthogonalVector2D(const Eigen::Vector2d& vector) {
-    Eigen::Vector2d orthogonalVector(-vector[1], vector[0]);
+    return Eigen::Vector2d(-vector[1], vector[0]);
+}
 
-    return orthogonalVector;
+/**
+*   @param vector1 2-vector
+*   @param vector2 2-vector
+*   @return the magnitude of the displacement vector between the two vectors
+*   @see 
+*/
+double distanceBetween(const Eigen::Vector2D& vector1, const Eigen::Vector2D& vector2) {
+    return (vector1 - vector2).norm();
+}
+
+/*  
+*   Chris thoughts --> I'm stupid so I thought about using convex linear combinations (I guess this is stil one), or adding half of the displacement vector between the two points.
+*   [TODO] - write desc.
+*
+*/
+Eigen::Vector2d midpoint(const Eigen::Vector2D& vector1, const Eigen::Vector2D& vector2) {
+    return (vector1 + vector2) / 2;
 }
 
 
 class Dubins {
     /*
         TODO: 
-            - implement a package that implements LINALG
             - copy the python code from the other obc repo
     */
 public:
-    Dubins(double radius, double point_separation) 
-        : radius(radius), 
-        point_separation(point_separation) 
-        {}
-    
+    Dubins(double radius, double point_separation)
+        : _radius(radius), _point_separation(point_separation) {
+        assert(radius > 0);
+        assert(point_separation > 0);
+    }
+ 
+    // [TODO] none of this is correct lol --> from ChatGPT
+    Eigen::Vector2d findCenter(const XYZCoord& point, char side) {
+        assert(side == 'L' || side == 'R');
+        double angle = point.psi + (side == 'L' ? M_PI / 2.0 : -M_PI / 2.0);
+        return Eigen::Vector2d(point.x + std::cos(angle) * _radius,
+                               point.y + std::sin(angle) * _radius);
+    }
+
+    // [TODO] check if works
+    Eigen::Vector2d circle_arc(const XYZCoord& starting_point, double beta, const Eigen::Vector2d& center, double path_length) {
+        double angle = starting_point.psi + ((path_length / _radius) - M_PI / 2) * std::copysign(1.0, beta);
+        Eigen::Vector2d vect(std::cos(angle), std::sin(angle));
+        return center + (vect * radius);
+    }   
 private:
-    double radius;
-    double point_seperation;
-    
+    double _radius;
+    double _point_seperation;
+
 };
 
 #endif // PATHING_DUBINS_HPP_
--- tests/dubins_test.cpp
+++ tests/dubins_test.cpp
@@ -1,11 +1,20 @@
 #include <gtest/gtest.h>
-
-
+#include "../include/pathing/dubins.hpp"
+#include "Eigen"
 /*
 *   
 *
 *
 */
 TEST(DubinsTest, Orthogonal2D) {
-    EXPECT_EQ(5,5);
+    Eigen::Vector2d input_vector1(1.0, 2.0);
+    Eigen::Vector2d input_vector2(-3.0, 4.0);
+    Eigen::Vector2d input_vector3(0.0, 0.0);
+
+    Eigen::Vector2d expected_output1(-2.0, 1.0);
+    Eigen::Vector2d expected_output2(-4.0, -3.0);
+    Eigen::Vector2d expected_output3(0.0, 0.0);
+
+    EXPECT_EQ(findOrthogonalVector2D(input_vector1), expected_output1);
 }
+
