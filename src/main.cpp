26203fb21008de9f4425edd80d80b28d168b6c1c
JasperHuang814 <jasper.huang6@gmail.com>
2023-11-01 23:58:57 +0000

    Update mavsdk

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,28 @@
 #include <iostream>
-#include "core/states.hpp"
+
 #include "Eigen"
+#include "core/states.hpp"
 // #include <opencv2/opencv.hpp>
+#include <mavsdk/mavsdk.h>
 #include <torch/torch.h>
+
 #include <iostream>
 
 // using namespace cv;
 
-int main()
-{
-  PreparationState state;
+int main() {
+    PreparationState state;
 
-  state.tick();
-  
-  return 0;
+    mavsdk::Mavsdk dc;
+    mavsdk::ConnectionResult conn_result =
+        dc.add_tcp_connection("127.0.0.1:5760");
+    // Wait for the system to connect via heartbeat
+    while (conn_result != mavsdk::ConnectionResult::Success) {
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        std::cout << "Adding connection failed: " << conn_result << '\n';
+    }
+
+    state.tick();
+
+    return 0;
 }
