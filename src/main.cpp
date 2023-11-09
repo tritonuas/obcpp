f73d8429bb8b05e6470a7d652a0b26f6ad7e56d2
Giovanni Clark <giclark@ucsd.edu>
2023-11-09 00:18:34 +0000

    add httplib library

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,17 @@
 // #include <opencv2/opencv.hpp>
 #include <torch/torch.h>
 #include <iostream>
+#include <httplib.h>
 
 // using namespace cv;
 
 int main()
 {
-  PreparationState state;
-
-  state.tick();
+    httplib::Server svr;
+    svr.Get("/hi", [](const httplib::Request &, httplib::Response &res) {
+        res.set_content("Hello World!", "text/plain");
+    });
+    svr.listen("0.0.0.0", 8080);
   
   return 0;
 }
