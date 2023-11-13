615acc14fbd969240894a1ee11a2d860b3f21fd7
AchuthKrishna <92132574+AchuthKrishna@users.noreply.github.com>
2023-11-12 18:47:14 -0800

    Add more simple tree tests
    Added simple tree tests for the remaining tree methods.

--- tests/tree_test.cpp
+++ tests/tree_test.cpp
@@ -23,3 +23,60 @@ TEST(SimpleTreeTest, addNodeTest) {
 
     EXPECT_TRUE(simpleTree.getEdge(point1, point2) != nullptr);
 }
+
+TEST(SimpleTreeTest, getNodeTest) {
+    RRTPoint point1 = RRTPoint(XYZCoord(1, 2, 0), 0);
+    RRTPoint point2 = RRTPoint(XYZCoord(0, 2, 0), 0);
+    RRTNode a = RRTNode(point1, 10);
+    RRTNode b = RRTNode(point2, 10);
+    std::vector<RRTPoint> path = {RRTPoint(XYZCoord(0.5, 2, 0), 0), RRTPoint(XYZCoord(0.25, 2, 0), 0)};
+    double edgeCost = 1;
+    RRTEdge edge = RRTEdge(&a, &b, path, edgeCost);
+    RRTTree simpleTree = RRTTree();
+    simpleTree.addNode(nullptr, &a, std::vector<RRTPoint>(), 0);
+    simpleTree.addNode(&a, &b, path, edgeCost);
+
+    EXPECT_TRUE(simpleTree.getNode(point1) != nullptr);
+    EXPECT_TRUE(simpleTree.getNode(point2) != nullptr);
+}
+
+TEST(SimpleTreeTest, getEdgeTest) {
+    RRTPoint point1 = RRTPoint(XYZCoord(1, 2, 0), 0);
+    RRTPoint point2 = RRTPoint(XYZCoord(0, 2, 0), 0);
+    RRTNode a = RRTNode(point1, 10);
+    RRTNode b = RRTNode(point2, 10);
+    std::vector<RRTPoint> path = {RRTPoint(XYZCoord(0.5, 2, 0), 0), RRTPoint(XYZCoord(0.25, 2, 0), 0)};
+    double edgeCost = 1;
+    RRTEdge edge = RRTEdge(&a, &b, path, edgeCost);
+    RRTTree simpleTree = RRTTree();
+    simpleTree.addNode(nullptr, &a, std::vector<RRTPoint>(), 0);
+    simpleTree.addNode(&a, &b, path, edgeCost);
+
+    EXPECT_TRUE(simpleTree.getEdge(point1, point2) != nullptr);
+}
+
+TEST(SimpleTreeTest, rewireEdgeTest) {
+    RRTPoint point1 = RRTPoint(XYZCoord(1, 2, 0), 0);
+    RRTPoint point2 = RRTPoint(XYZCoord(0, 2, 0), 0);
+    RRTNode a = RRTNode(point1, 10);
+    RRTNode b = RRTNode(point2, 10);
+    std::vector<RRTPoint> path = {RRTPoint(XYZCoord(0.5, 2, 0), 0), RRTPoint(XYZCoord(0.25, 2, 0), 0)};
+    double edgeCost = 1;
+    RRTEdge edge = RRTEdge(&a, &b, path, edgeCost);
+    RRTTree simpleTree = RRTTree();
+
+    simpleTree.addNode(nullptr, &a, std::vector<RRTPoint>(), 0);
+    simpleTree.addNode(&a, &b, path, edgeCost);
+
+    RRTPoint point3 = RRTPoint(XYZCoord(3,2,0), 0);
+    RRTNode c = RRTNode(point3, 10);
+
+    simpleTree.rewireEdge(&a, &b, &c, path, edgeCost);
+
+    EXPECT_TRUE(a.getReachable().back() == &c);
+    EXPECT_TRUE(std::count(a.getReachable().begin(), a.getReachable().end(), b) == 0);
+
+    EXPECT_TRUE(simpleTree.getNode(point2) == nullptr);
+    EXPECT_TRUE(simpleTree.getNode(point3) != nullptr);
+    EXPECT_TRUE(simpleTree.getEdge(point1, point3) != nullptr);
+}
