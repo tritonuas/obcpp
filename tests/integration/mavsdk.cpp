f1b4d66225f62a2340df2adb83bb52ea94956be7
JasperHuang814 <jasper.huang6@gmail.com>
2023-11-15 23:55:55 +0000

    Update integration test and cmake for mavsdk

--- tests/integration/mavsdk.cpp
+++ tests/integration/mavsdk.cpp
@@ -3,10 +3,18 @@
 #include <iostream>
 #include <mavsdk/mavsdk.h>
 
+
+/*
+* Test mavsdk connection to an endpoint 
+* bin/mavsdk [connection_link]
+* connection_link -> [protocol]://[ip]:[port]
+* example: 
+*   bin/mavsdk tcp://127.0.0.1:5760
+*/ 
 int main(int argc, char *argv[]) {
     
     mavsdk::Mavsdk dc;
-    mavsdk::ConnectionResult conn_result = dc.add_tcp_connection(argv[1]);
+    mavsdk::ConnectionResult conn_result = dc.add_any_connection(argv[1]);
     
     // Wait for the system to connect via heartbeat
     if (conn_result != mavsdk::ConnectionResult::Success) {
