ba56ffec2061ff394540c969da10af7b655be4f4
Samir Rashid <samirsgotmail@gmail.com>
2023-11-17 07:03:36 +0000

    improve all cmakes

--- deps/matplot/matplot.cmake
+++ deps/matplot/matplot.cmake
@@ -1,9 +1,15 @@
 function(target_add_matplot target_name)
     include(FetchContent)
 
+    # TODO: Why is this download so slow?
+    # The binaries on the release page are 4MB, we should use those.
     FetchContent_Declare(matplotplusplus
-            GIT_REPOSITORY https://github.com/alandefreitas/matplotplusplus
-            GIT_TAG origin/master) # or whatever tag you want
+            URL https://github.com/alandefreitas/matplotplusplus/archive/refs/tags/v1.2.0.zip
+            URL_HASH SHA256=282446b30a285ea593ee7baaf6fb8978551fd3671e78b1b869270a62d1decf3b
+            DOWNLOAD_EXTRACT_TIMESTAMP true
+            GIT_PROGRESS TRUE
+            GIT_SHALLOW TRUE
+    )
             
     FetchContent_GetProperties(matplotplusplus)
     if(NOT matplotplusplus_POPULATED)
