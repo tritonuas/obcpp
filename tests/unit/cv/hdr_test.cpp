92b3112f81265d1105e7c9e51a4c15f574c65317
Samir Rashid <samirsgotmail@gmail.com>
2023-11-21 20:14:46 +0000

    fix: HDR test passes without images

--- tests/unit/cv/hdr_test.cpp
+++ tests/unit/cv/hdr_test.cpp
@@ -1,7 +1,7 @@
-#include <gtest/gtest.h>
 #include "cv/hdr.hpp"
+#include <gtest/gtest.h>
 
-TEST(HdrTest,compute_hdr_test) {
+TEST(HdrTest, compute_hdr_test) {
   int hdr_error_code = compute_hdr();
   EXPECT_EQ(0, hdr_error_code);
 }
