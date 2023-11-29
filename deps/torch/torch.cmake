045fdf47ca9575fa0c08b17823c5dda296983d5c
Anthony Tarbinian <atar137h@gmail.com>
2023-11-29 15:09:47 -0800

    install torch/torchvision in docker and add required linker flags
    torch and torchvision are now installed in the docker image instead of
    via CMake FetchContent.
    
    also added a linker flag when linking torchvision that's required. See
    this file for a reference: https://github.com/facebookresearch/detectron2/blob/main/tools/deploy/CMakeLists.txt#L21
    
    also added load_torchvision_model integration test file

--- deps/torch/torch.cmake
+++ deps/torch/torch.cmake
@@ -1,18 +1,9 @@
 function(target_add_torch target_name)
     message("Adding torch as a dependency for target ${target_name}")
 
-    include(FetchContent)
-    Set(FETCHCONTENT_QUIET FALSE)
-
-    FetchContent_Declare(Torch
-      URL https://download.pytorch.org/libtorch/cpu/libtorch-cxx11-abi-shared-with-deps-2.1.0%2Bcpu.zip
-    )
-
-    FetchContent_MakeAvailable(Torch)
-    find_package(Torch REQUIRED HINTS "${torch_SOURCE_DIR}")
+    # specify a CMAKE_PREFIX_PATH that points to torch installation
+    find_package(Torch REQUIRED)
     set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${TORCH_CXX_FLAGS}")
-    target_link_libraries(${target_name} PRIVATE
-        "${TORCH_LIBRARIES}"
-    )
+    target_link_libraries(${target_name} PRIVATE ${TORCH_LIBRARIES})
     target_include_directories(${target_name} PRIVATE ${TORCH_INCLUDE_DIRS})
 endfunction()
