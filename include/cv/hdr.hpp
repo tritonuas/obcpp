d226d73eee17a735296d63b2fbf236ef3531488b
Samir Rashid <samirsgotmail@gmail.com>
2023-11-30 00:31:55 +0000

    separate out HDR logic

--- include/cv/hdr.hpp
+++ include/cv/hdr.hpp
@@ -9,6 +9,12 @@
 // #include <opencv2/photo.hpp>
 // #include <opencv2/imgproc.hpp>
 
-int compute_hdr();
+/**
+ * Generate images for testing.
+*/
+std::vector<cv::Mat>  generate_test_images();
+std::vector<cv::Mat> load_images();
+int compute_hdr(std::vector<cv::Mat> img_list, const cv::Mat& exposure_times);
+cv::Mat get_image_exposure_times();
 
 #endif
