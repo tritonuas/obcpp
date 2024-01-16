cad6eaab51b272c0061f31986ef793514b141222
Tyler Lentz <tlentz@ucsd.edu>
2024-01-16 23:34:20 +0000

    gonna kms

--- scripts/install-protos.sh
+++ scripts/install-protos.sh
@@ -2,9 +2,9 @@
 
 # This script will be run from inside the build directory because it is executed from 
 # CMake, which we run from inside the build directory
-if [ "$GITHUB_ACTIONS" != "true" ]; then
-    git submodule update --init --remote
-fi
+# if [ "$GITHUB_ACTIONS" != "true" ]; then
+#     git submodule update --init --remote
+# fi
 
 # Compile both of the protobuf files into the build directory
 mkdir gen_protos
