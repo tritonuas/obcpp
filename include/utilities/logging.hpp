e91089a02eb6327f81890b481d10bf393a7c5137
Tyler Lentz <tlentz@ucsd.edu>
2024-01-31 08:17:46 +0000

    add logging settings & file IO

---
+++ include/utilities/logging.hpp
@@ -0,0 +1,10 @@
+#ifndef INCLUDE_UTILITIES_LOGGING_HPP_
+#define INCLUDE_UTILITIES_LOGGING_HPP_
+
+#include <string>
+
+time_t getUnixTime();
+
+std::string getLoggingFilename(int argc, char* argv[]);
+
+#endif
