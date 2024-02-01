14a6377d93dc29df8c1837d8807971bbba73e193
Tyler Lentz <tlentz@ucsd.edu>
2024-02-01 06:32:43 +0000

    make loguru download specific commit hash & use SHA256 hash to validate it

--- deps/loguru/loguru.cmake
+++ deps/loguru/loguru.cmake
@@ -1,12 +1,12 @@
 function(target_add_loguru target_name)
     include(FetchContent)
     FetchContent_Declare(LoguruGitRepo
-        GIT_REPOSITORY "https://github.com/emilk/loguru" # can be a filesystem path
-        GIT_TAG        "master"
+        URL "https://github.com/emilk/loguru/archive/4adaa185883e3c04da25913579c451d3c32cfac1.zip"
+        URL_HASH SHA256=da272cae7a177217a02fbb2622bd0ce80f3b0b6a4ffb71579c186d80ecf81f2e
+        DOWNLOAD_EXTRACT_TIMESTAMP true
     )
 
     # set any loguru compile-time flags before calling MakeAvailable()
-    set(LOGURU_WITH_STREAMS TRUE)
     FetchContent_MakeAvailable(LoguruGitRepo) # defines target 'loguru::loguru'
 
     target_link_libraries(${target_name} PRIVATE
