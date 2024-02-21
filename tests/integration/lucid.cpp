a6f788a85c97a09d74fd31568027dd3d425a2543
Boris Ryabov <stonks33@gmail.com>
2024-02-20 20:09:23 -0800

    quickfix

--- tests/integration/lucid.cpp
+++ tests/integration/lucid.cpp
@@ -8,10 +8,29 @@ using json = nlohmann::json;
 
 int main() 
 {
-    std::ifstream f("home/tritonuas/boris-lucid-obcpp/tests/integration/util/config.json");
-    std::cout << "Reading json file." << std::endl;
-    std::cout << f.rdbuf() << std::endl;
-    json config = json::parse(f);
+
+    json config = json::parse(R"(
+{
+    "TriggerSelector":"FrameStart",
+    "TriggerMode":"On",
+    "TriggerSource":"Software",
+    "SensorShutterMode":"Rolling",
+    "ExposureAuto":"Continuous",
+    "AcquisitionFrameRateEnable":true,
+    "TargetBrightness":70,
+    "GammaEnable":true,
+    "Gamma":0.5,
+    "GainAuto":"Continuous",
+    "ExposureAutoDamping":1,
+    "ExposureAutoAlgorithm":"Median",
+    "ExposureAutoUpperLimit":500,
+    "ExposureAutoLowerLimit":360,
+    "GainAutoUpperLimit":10,
+    "GainAutoLowerLimit":1,
+    "StreamAutoNegotiatePacketSize":true,  
+    "StreamPacketResendEnable":true
+}
+)");
     LucidCameraConfig * lucidConfig = new LucidCameraConfig(config);
 
     LucidCamera camera = LucidCamera(lucidConfig);
