d0ce19fdc65783f4ab7b9e29095ebb71bea52956
Boris Ryabov <stonks33@gmail.com>
2024-02-20 19:21:20 -0800

    [TEST] test config reads

--- tests/integration/lucid.cpp
+++ tests/integration/lucid.cpp
@@ -3,10 +3,15 @@
 #include <unordered_map>
 #include <optional>
 #include "camera/lucid.hpp"
+#include <fstream>
 using json = nlohmann::json;
 
 int main() 
 {
-    LucidCamera camera = LucidCamera(nullptr);
+    std::ifstream ifs("tests/integration/util/config.json");
+    json config = json::parse(ifs);
+    LucidCameraConfig * lucidConfig = new LucidCameraConfig(config);
+
+    LucidCamera camera = LucidCamera(lucidConfig);
     camera.connect();
 }
