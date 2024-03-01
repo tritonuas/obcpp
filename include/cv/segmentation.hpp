0d479ae90848e24e32547958dc4ac949e8c12eae
shijiew555 <shijiew21@gmail.com>
2024-02-29 16:15:51 -0800

    finished integrating segmentation

--- include/cv/segmentation.hpp
+++ include/cv/segmentation.hpp
@@ -3,6 +3,10 @@
 
 #include <opencv2/opencv.hpp>
 
+#include <torch/script.h>
+#include <iostream>
+#include <memory>
+
 #include "cv/utilities.hpp"
 
 struct SegmentationResults {
@@ -20,8 +24,30 @@ struct SegmentationResults {
 // for segmentation models can be found here:
 // https://github.com/tritonuas/hutzler-571
 class Segmentation {
- public:
-    SegmentationResults segment(const CroppedTarget &target);
+    public:
+        Segmentation() {
+            // Initialize without a model or set up default values
+        }
+
+        Segmentation(const std::string &modelPath) {
+            try {
+                this->module = torch::jit::load(modelPath);
+            }
+            catch (const c10::Error& e) {
+                std::cerr << "ERROR: could not load the model, check if model file is present in /bin\n";
+                throw;
+            }
+        }
+        SegmentationResults segment(const CroppedTarget &target);
+    private:
+        torch::jit::script::Module module;
 };
+
+std::string get_image_type(const cv::Mat& img, bool more_info);
+void show_image(cv::Mat& img, std::string title);
+at::Tensor transpose(at::Tensor tensor, c10::IntArrayRef dims);
+std::vector<torch::jit::IValue> ToInput(at::Tensor tensor_image);
+at::Tensor ToTensor(cv::Mat img, bool show_output, bool unsqueeze, int unsqueeze_dim);
+cv::Mat ToCvImage(at::Tensor tensor);
 
 #endif  // INCLUDE_CV_SEGMENTATION_HPP_
---
+++ src/cv/segmentation.cpp
@@ -0,0 +1,114 @@
+#include "cv/segmentation.hpp"
+
+SegmentationResults Segmentation::segment(const CroppedTarget &target) {
+    auto device = torch::kCPU;
+    auto fcn = this->module;
+    fcn.to(device);
+
+    auto target_tensor = ToTensor(target.croppedImage, false, false, 0);
+    target_tensor = target_tensor.clamp_max(c10::Scalar(50)); 
+    target_tensor = target_tensor.toType(c10::kFloat).div(255);
+    target_tensor = transpose(target_tensor, { (2),(0),(1) });
+    target_tensor.unsqueeze_(0);
+    
+    auto input_to_net = ToInput(target_tensor);
+    at::Tensor output = (fcn.forward(input_to_net).toGenericDict().begin()->value()).toTensor();
+    torch::Tensor cpu_tensor = output.to(device); 
+    cpu_tensor = cpu_tensor[0];
+    cpu_tensor = torch::softmax(cpu_tensor, 0);
+
+    auto character_mask = cpu_tensor[1];
+    auto shape_mask = cpu_tensor[0];
+    cv::Mat character_mask_mat(character_mask.sizes()[0], character_mask.sizes()[1], 
+        CV_32FC1, character_mask.data_ptr<float>());
+    cv::Mat shape_mask_mat(shape_mask.sizes()[0], shape_mask.sizes()[1], 
+        CV_32FC1, shape_mask.data_ptr<float>());
+
+    SegmentationResults result;
+    result.shapeMask = character_mask_mat;
+    result.characterMask = shape_mask_mat;
+    return result;
+}
+
+
+
+std::string get_image_type(const cv::Mat& img, bool more_info=true) 
+{
+    std::string r;
+    int type = img.type();
+    uchar depth = type & CV_MAT_DEPTH_MASK;
+    uchar chans = 1 + (type >> CV_CN_SHIFT);
+
+    switch (depth) {
+    case CV_8U:  r = "8U"; break;
+    case CV_8S:  r = "8S"; break;
+    case CV_16U: r = "16U"; break;
+    case CV_16S: r = "16S"; break;
+    case CV_32S: r = "32S"; break;
+    case CV_32F: r = "32F"; break;
+    case CV_64F: r = "64F"; break;
+    default:     r = "User"; break;
+    }
+
+    r += "C";
+    r += (chans + '0');
+   
+    if (more_info)
+        std::cout << "depth: " << img.depth() << " channels: " << img.channels() << std::endl;
+
+    return r;
+}
+
+void show_image(cv::Mat& img, std::string title)
+{
+    std::string image_type = get_image_type(img);
+    cv::namedWindow(title + " type:" + image_type, cv::WINDOW_NORMAL); // Create a window for display.
+    cv::imshow(title, img);
+    cv::waitKey(0);
+}
+
+at::Tensor transpose(at::Tensor tensor, c10::IntArrayRef dims = { 0, 3, 1, 2 })
+{
+    tensor = tensor.permute(dims);
+    return tensor;
+}
+
+at::Tensor ToTensor(cv::Mat img, bool show_output = false, bool unsqueeze=false, int unsqueeze_dim = 0)
+{
+    at::Tensor tensor_image = torch::from_blob(img.data, { img.rows, img.cols, 3 }, at::kByte);
+
+    if (unsqueeze)
+    {
+        tensor_image.unsqueeze_(unsqueeze_dim);
+    }
+    
+    if (show_output)
+    {
+        std::cout << tensor_image.slice(2, 0, 1) << std::endl;
+    }
+    return tensor_image;
+}
+
+std::vector<torch::jit::IValue> ToInput(at::Tensor tensor_image)
+{
+    // Create a vector of inputs.
+    return std::vector<torch::jit::IValue>{tensor_image};
+}
+
+cv::Mat ToCvImage(at::Tensor tensor)
+{
+    int width = tensor.sizes()[0];
+    int height = tensor.sizes()[1];
+    try
+    {
+        cv::Mat output_mat(cv::Size{ height, width }, CV_8UC3, tensor.data_ptr<uchar>());
+        
+        show_image(output_mat, "converted image from tensor");
+        return output_mat.clone();
+    }
+    catch (const c10::Error& e)
+    {
+        std::cout << "an error has occured : " << e.msg() << std::endl;
+    }
+    return cv::Mat(height, width, CV_8UC3);
+}
