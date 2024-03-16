75e7531a9c85bad16d357ec7bb7dbdb7db793650
mobslayer202 <gavinlyuan@gmail.com>
2024-03-16 20:41:10 +0000

    Loaded in user specified JSON, Got Values
    Additionally:
    -Did fix Anthony said for Magick++.h no such file or directory for Playground and MavlinkClient to get it to make
    -Some modification in protos which I don't remember touching

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <chrono>
 #include <string>
+#include <fstream>
 
+#include "nlohmann/json.hpp"
 extern "C" {
     #include "network/airdrop_sockets.h"
 }
@@ -9,10 +11,25 @@ extern "C" {
 #include "utilities/constants.hpp"
 #include "utilities/logging.hpp"
 
+using json = nlohmann::json;
 
 int main(int argc, char* argv[]) {
     // TODO: pull logging folder from config
     initLogging("/workspaces/obcpp/logs", true, argc, argv);
+
+    // START: My Code 
+    LOG_F(INFO, "HEEEELLLLLOOOOOOOOO");
+
+    // Load user specified config json
+    std::string fileName = argv[1];
+    std::ifstream configStream("/workspaces/obcpp/configs/" + fileName);
+    json config = json::parse(configStream);
+
+    // Get values
+    std::string mavlinkIP = config["network"]["mavlink"]["connect"];
+    int gcsPORTInt = config["network"]["gcs"]["port"];
+    LOG_F(INFO, mavlinkIP.c_str());
+    LOG_F(INFO, std::to_string(gcsPORTInt).c_str());
 
     // In future, load configs, perhaps command line parameters, and pass
     // into the obc object
