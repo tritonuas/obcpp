c831a6a5b8edb4ea6f0f9edcd0b7245b631eb24d
Tyler Lentz <tlentz@ucsd.edu>
2024-04-17 20:00:03 +0000

    remove testing stuff

--- src/ticks/mission_prep.cpp
+++ src/ticks/mission_prep.cpp
@@ -18,10 +18,6 @@ std::chrono::milliseconds MissionPrepTick::getWait() const {
 using namespace std::chrono_literals;
 
 Tick* MissionPrepTick::tick() {
-    if (this->state->getAirdrop() != nullptr) {
-        LOG_F(INFO, "%ld", this->state->getAirdrop()->getLostConnections(1s).size());
-    }
-
     if (this->state->config.getCachedMission().has_value()) {
         LOG_F(INFO, "Valid mission configuration detected");
         return new PathGenTick(this->state);
