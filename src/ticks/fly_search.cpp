53506400301714d3b25916dcc147c4cbe4a65bb6
miyatakazuya <kazuyamiyata18@gmail.com>
2024-04-29 14:49:54 -0700

    Added function prototype

--- src/ticks/fly_search.cpp
+++ src/ticks/fly_search.cpp
@@ -6,12 +6,26 @@
 #include "utilities/constants.hpp"
 
 FlySearchTick::FlySearchTick(std::shared_ptr<MissionState> state)
-    :Tick(state, TickID::FlySearch) {}
+    :Tick(state, TickID::FlySearch) {
+        const std::chrono::milliseconds interval {2000};
+        //this.state.camera.startTakingPictures(&interval) = 0;
+    }
 
 std::chrono::milliseconds FlySearchTick::getWait() const {
     return FLY_SEARCH_TICK_WAIT;
 }
 
 Tick* FlySearchTick::tick() {
-    return nullptr;
+
+    // Run Mavlink flight to find targets
+    // Take Photos
+    // Finish Loop
+
+   
+    // Deconstructor
+    if (false) {
+        getAllImages
+        return new CVLoiterTick(this->state);
+    }
+    //return nullptr;
 }
