a35a54df4bed4611aa328a5d5b5694f2fb72e066
miyatakazuya <kazuyamiyata18@gmail.com>
2024-05-02 19:39:45 -0700

    Added CVLoiter constructor

--- src/ticks/cv_loiter.cpp
+++ src/ticks/cv_loiter.cpp
@@ -4,9 +4,19 @@
 
 #include "ticks/ids.hpp"
 #include "utilities/constants.hpp"
+#include <opencv2/opencv.hpp>
+#include <loguru.hpp>
+
+#include "cv/pipeline.hpp"
+#include "cv/aggregator.hpp"
 
 CVLoiterTick::CVLoiterTick(std::shared_ptr<MissionState> state)
-    :Tick(state, TickID::CVLoiter) {}
+    :Tick(state, TickID::CVLoiter) {
+        // Get the airdrop bottles from the mission parameters
+        std::array<Bottle> = state.mission_params.getAirdropBottles();
+        // Get the images from the camera
+        std::vector<ImageData> images = this.state.camera.getAllImages();
+    }
 
 std::chrono::milliseconds CVLoiterTick::getWait() const {
     return CV_LOITER_TICK_WAIT;
@@ -14,6 +24,7 @@ std::chrono::milliseconds CVLoiterTick::getWait() const {
 
 Tick* CVLoiterTick::tick() {
     //Tick is called if Search Zone coverage path is finished
+
 
     //Check if all expected targets are found 
     if (false) {
@@ -23,5 +34,6 @@ Tick* CVLoiterTick::tick() {
     else if (false) {
         return new FlySearchTick(this->state);
     }
+
     return nullptr;
 }
