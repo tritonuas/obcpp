eb12c8ff46eb7b2a3245c90d3bd563b04eda5114
MatthewHsu1 <matthewhsu1128@gmail.com>
2024-06-05 16:53:05 -0700

    Prevent accidentally clicking takeoff twice and seg fault.

--- src/network/gcs_routes.cpp
+++ src/network/gcs_routes.cpp
@@ -257,7 +257,7 @@ DEF_GCS_HANDLE(Post, takeoff, manual) {
     LOG_REQUEST("POST", "takeoff/manual");
 
     auto lock_ptr = state->getTickLockPtr<WaitForTakeoffTick>();
-    if (!lock_ptr.has_value()) {
+    if (!lock_ptr.has_value() || state->getTickID() != TickID::WaitForTakeoff) {
         LOG_RESPONSE(WARNING, "Not currently in WaitForTakeoff Tick", BAD_REQUEST);
         return;
     }
@@ -269,7 +269,7 @@ DEF_GCS_HANDLE(Post, takeoff, autonomous) {
     LOG_REQUEST("POST", "takeoff/autonomous");
 
     auto lock_ptr = state->getTickLockPtr<WaitForTakeoffTick>();
-    if (!lock_ptr.has_value()) {
+    if (!lock_ptr.has_value() || state->getTickID() != TickID::WaitForTakeoff) {
         LOG_RESPONSE(WARNING, "Not currently in WaitForTakeoff Tick", BAD_REQUEST);
         return;
     }
