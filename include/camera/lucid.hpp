b45a10a1c3a62ac13d4a0c2f8daabbfae62e3170
MatthewHsu1 <matthewhsu1128@gmail.com>
2024-06-11 23:21:36 +0000

    Resolve merge conflicts

--- include/camera/lucid.hpp
+++ include/camera/lucid.hpp
@@ -74,10 +74,6 @@ class LucidCamera : public CameraInterface {
     */
     std::optional<ImageData> takePicture(const std::chrono::milliseconds& timeout,
         std::shared_ptr<MavlinkClient> mavlinkClient) override;
-<<<<<<< HEAD
-=======
-
->>>>>>> main
     void startStreaming() override;
 
  private:
