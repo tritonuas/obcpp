cd32ee7f7a219de60047952dc20e167feae28fe6
miyatakazuya <kazuyamiyata18@gmail.com>
2024-11-13 19:46:22 -0800

    added getaddrinfo

--- include/utilities/tcp.hpp
+++ include/utilities/tcp.hpp
@@ -5,19 +5,24 @@
 
 
 #define PORT "4345"
-
 #define MAX_MSG_SIZE 100
+
+/*
+Reference: https://www.beej.us/guide/bgnet/html/
+*/
 
 class TCPClient{
  public:
     TCPClient(std::string host, uint16_t port);
     ~TCPClient();
  private:
-    int sockfd;
-    struct addrinfo hints, *servinfo, *p;
     std::mutex server_mut;
+    int sockfd, numbytes;
+    struct addrinfo hints, *servinfo, *p;
     int rv;
     char s[INET6_ADDRSTRLEN];
+
+    void *get_in_addr(struct sockaddr *sa);
 }
 
 // https://man7.org/linux/man-pages/man2/connect.2.html
--- src/utilities/tcp.cpp
+++ src/utilities/tcp.cpp
@@ -1,5 +1,17 @@
 #include "utilities/tcp.hpp"
 
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET) {
+        return &(((struct sockaddr_in*)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+
 TCPClient::TCPClient(std::string host, uint16_t port){
     
 }
