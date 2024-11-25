e6da801003d6211e982df574d666ff1db084e520
miyatakazuya <kazuyamiyata18@gmail.com>
2024-11-24 20:11:28 -0800

    modified tcp constructor

--- include/utilities/tcp.hpp
+++ include/utilities/tcp.hpp
@@ -1,3 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <netdb.h>
+#include <netinet/in.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <netdb.h>
--- src/utilities/tcp.cpp
+++ src/utilities/tcp.cpp
@@ -1,6 +1,5 @@
 #include "utilities/tcp.hpp"
 
-
 // get sockaddr, IPv4 or IPv6:
 void *get_in_addr(struct sockaddr *sa)
 {
@@ -12,6 +11,15 @@ void *get_in_addr(struct sockaddr *sa)
 }
 
 
+
 TCPClient::TCPClient(std::string host, uint16_t port){
-    
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+
+
+}
+
+TCPClient::~TCPClient(){
+    close(sockfd);
 }
