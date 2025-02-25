efe4aa100372f44513c51ca77db0cdebd7e8a96c
Boris Ryabov <stonks33@gmail.com>
2025-02-25 14:27:52 -0800

    [add] implementation files

---
+++ src/network/camera_client.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <boost/array.hpp>
+#include <boost/asio.hpp>
+#include "camera_data.hpp"
+
+// https://www.codeproject.com/Articles/1264257/Socket-Programming-in-Cplusplus-using-boost-asio-T
+using namespace boost::asio;
+using ip::tcp;
