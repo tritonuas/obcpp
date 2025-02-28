9d082dfd7da27cae185bd20c90713f677a547e7b
Boris Ryabov <stonks33@gmail.com>
2025-02-28 12:27:23 -0800

    [ADD] changed headers, constr and connect funcs in cpp

--- include/network/camera_client.hpp
+++ include/network/camera_client.hpp
@@ -1,20 +1,19 @@
 #include <iostream>
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
-#include "camera_data.hpp"
-
+#include "network/camera_data.hpp"
 // https://www.codeproject.com/Articles/1264257/Socket-Programming-in-Cplusplus-using-boost-asio-T
-using namespace boost::asio;
-using ip::tcp;
+namespace asio = boost::asio;
 
 class CameraClient {
     public:
         /*
          * Create the socket
          * Params:
+         *   - io_context (boost::asio::io_context)  - what io context to use
          *   - port (int)  - which port to occupy
          */
-        explicit CameraClient(int port);
+        CameraClient(int port);
         ~CameraClient();
 
         /*
@@ -23,7 +22,7 @@ class CameraClient {
          *   - IP (string) - regular ip
          *   - port (int)  - port
          */
-        bool connect(std::string ip, int port);
+        bool connect(asio::io_context io_context, std::string ip, int port);
 
         /*
          * Sends pics out
@@ -38,8 +37,9 @@ class CameraClient {
         CameraResponse_t read();
 
     private:
+
         // ig?
         string ip;
         int port;
-        tcp::socket socket;
+        asio::tcp::socket socket;
 };
--- src/network/camera_client.cpp
+++ src/network/camera_client.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 #include <boost/array.hpp>
 #include <boost/asio.hpp>
-#include "camera_data.hpp"
+#include "network/camera_data.hpp"
+#include "network/camera_client.hpp"
+namespace asio = boost::asio;
 
-// https://www.codeproject.com/Articles/1264257/Socket-Programming-in-Cplusplus-using-boost-asio-T
-using namespace boost::asio;
-using ip::tcp;
+CameraClient::CameraClient(int port)
+{
+    this->port=port;
+}
+
+CameraClient::connect(asio::io_context * io_context, std::string ip, int port)
+{
+    // create an asio endpoint
+    asio::ip::tcp::endpoint endpoint(asio::make_adress(ip), port);
+
+    // create a socket
+    this->socket(&io_context);
+
+    // connect socket
+    this->socket::connect(&endpoint);
+}
