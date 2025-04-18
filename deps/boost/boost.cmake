6e3b2569e4c3dd132edfa603c183adc412e8b71b
Christopher Lee <chrisdenglee@gmail.com>
2025-04-17 18:01:13 -0700

    change boost import

--- deps/boost/boost.cmake
+++ deps/boost/boost.cmake
@@ -7,8 +7,10 @@ function(target_add_boost target_name)
         DOWNLOAD_EXTRACT_TIMESTAMP true
     )
     FetchContent_MakeAvailable(boost)
-    target_link_libraries(${target_name} PRIVATE
-        boost
-    )
+    # target_link_libraries(${target_name} PRIVATE
+    #     boost
+    # )
+    target_include_directories(${target_name} PRIVATE ${boost_SOURCE_DIR})
+
 
 endfunction()
