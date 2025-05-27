c77ddc98652e528b8d1b4019b99a18ad81bce097
Triton UAS <ucsdtuas@gmail.com>
2025-05-26 17:15:01 -0700

    fix: drop_it_like_its_hot update mavlink constructor

--- tests/integration/drop_it_like_its_hot.cpp
+++ tests/integration/drop_it_like_its_hot.cpp
@@ -1,48 +1,35 @@
-#include <chrono>
-#include <cmath>
-#include <iostream>
-#include <memory>
-#include <nlohmann/json.hpp>
-#include <thread>
-#include <unordered_map>
-
-#include "core/mission_state.hpp"
-#include "handler_params.hpp"
-#include "network/gcs.hpp"
-#include "network/gcs_macros.hpp"
-#include "network/gcs_routes.hpp"
-#include "pathing/plotting.hpp"
-#include "pathing/static.hpp"
+#include "network/mavlink.hpp"
 #include "ticks/airdrop_approach.hpp"
-#include "ticks/mav_upload.hpp"
-#include "ticks/mission_prep.hpp"
-#include "ticks/path_gen.hpp"
-#include "ticks/tick.hpp"
-#include "utilities/constants.hpp"
-#include "utilities/datatypes.hpp"
-#include "utilities/http.hpp"
+#include "utilities/logging.hpp"
 
-// Tries to drop airdrop thru mavlink with relay 2
+/** 
+ * To run, call `bin/drop_it_like_its_hot [CONFIG]`.
+ * 
+ * DROP IT LIKE ITS HOT is the test for airdrop signal. For the 2025 
+ * competition, our airdrop mechanism is a electro-pelectro-permanent
+ * magnet that is triggered by the pixhawk. Therefore, we send a singal to the
+ * Pixhawk to drop the payload.
+ * 
+ * This is to test if the signal is sent successfully (we should set up the 
+ * apparatus and see if the epm toggles).
+ * 
+ * For detailed setup, go look at `mavlink_client.cpp`.
+*/
 int main(int argc, char* argv[]) {
-    // DECLARE_HANDLER_PARAMS(state, req, resp);
-    // state->setTick(new MissionPrepTick(state));
-
-    if (argc != 2) {
-        LOG_F(ERROR, "Expected use: bin/mavsdk [connection]");
+    if (argc != 5) {
+        LOG_F(ERROR, "Expected use: bin/mavsdk [config]");
         return 1;
     }
 
-    LOG_S(INFO) << "Attempting to connect at " << argv[1];
+    // connection to mav
+    std::shared_ptr<MavlinkClient> mav_ptr(new MavlinkClient(OBCConfig(argc, argv)));
 
-    MavlinkClient mav(argv[1]);
-
-    std::shared_ptr<MavlinkClient> mav_ptr = std::make_shared<MavlinkClient>(argv[1]);
-
+    // dummy info
     std::optional<airdrop_t> next_airdrop_to_drop;
     AirdropIndex next_airdrop = static_cast<AirdropIndex>(2);
-
     next_airdrop_to_drop = static_cast<airdrop_t>(next_airdrop);
 
+    // drop
     triggerAirdrop(mav_ptr, next_airdrop_to_drop.value());
 
     return 0;
