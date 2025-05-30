dc7d8d4cb51584e9bf8f09b77c9127d959797682
Kimi Chen <kimichen1115@gmail.com>
2025-05-30 20:48:20 +0000

    lint

--- src/ticks/manual_landing.cpp
+++ src/ticks/manual_landing.cpp
@@ -14,36 +14,35 @@ ManualLandingTick::ManualLandingTick(std::shared_ptr<MissionState> state, Tick* 
 std::chrono::milliseconds ManualLandingTick::getWait() const { return MANUAL_LANDING_TICK_WAIT; }
 
 Tick* ManualLandingTick::tick() {
-
     if (state->getDroppedAirdrops().size() >= NUM_AIRDROPS) {
         if (state->getMappingIsDone() == false) {
             // Currently does a two pass at the end
             // Probably should update for next year to optimize it.
-    
+
             cv::Stitcher::Mode scan_mode = cv::Stitcher::SCANS;
             // Direct stitching of all images in the mapping folder
-    
+
             fs::path base_dir = "../images/mapping";
             fs::path output_dir = base_dir / "output";
-    
+
             // Make sure the output directory exists (create if it doesn't)
             if (!fs::exists(output_dir)) {
                 fs::create_directories(output_dir);
             }
-    
+
             Mapping mapper;
             const int chunk_size = 5;
             const int chunk_overlap = 2;
             const int max_dim = 3000;
-    
+
             std::cout << "First pass stitching..." << std::endl;
             mapper.firstPass(base_dir.string(), output_dir.string(), chunk_size,
             chunk_overlap, scan_mode, max_dim, true);
-    
+
             std::cout << "Second pass stitching..." << std::endl;
             mapper.secondPass(output_dir.string(), scan_mode, max_dim, true);
             state->setMappingIsDone(true);
-    
+
             std::cout << "Mapping complete." << std::endl;
         }
     } else {
