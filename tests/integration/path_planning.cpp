8da81bafea4cee9f598d10f645d5ebb5b4a5fd85
AskewParity <chrisdenglee@gmail.com>
2025-07-14 18:47:45 -0400

    qol: move mission data for path_planning to file

--- tests/integration/path_planning.cpp
+++ tests/integration/path_planning.cpp
@@ -1,5 +1,6 @@
 #include <chrono>
 #include <cmath>
+#include <fstream>
 #include <iostream>
 #include <iomanip>
 #include <memory>
@@ -24,180 +25,6 @@
 
 #include "handler_params.hpp"
 
-const static char* mission_json_2020 = R"(
-{
-  "FlightBoundary": [
-        {
-          "Latitude": 38.1462694444444,
-          "Longitude": -76.4281638888889
-        },
-        {
-          "Latitude": 38.151625,
-          "Longitude": -76.4286833333333
-        },
-        {
-          "Latitude": 38.1518888888889,
-          "Longitude": -76.4314666666667
-        },
-        {
-          "Latitude": 38.1505944444444,
-          "Longitude": -76.4353611111111
-        },
-        {
-          "Latitude": 38.1475666666667,
-          "Longitude": -76.4323416666667
-        },
-        {
-          "Latitude": 38.1446666666667,
-          "Longitude": -76.4329472222222
-        },
-        {
-          "Latitude": 38.1432555555556,
-          "Longitude": -76.4347666666667
-        },
-        {
-          "Latitude": 38.1404638888889,
-          "Longitude": -76.4326361111111
-        },
-        {
-          "Latitude": 38.1407194444444,
-          "Longitude": -76.4260138888889
-        },
-        {
-          "Latitude": 38.1437611111111,
-          "Longitude": -76.4212055555556
-        },
-        {
-          "Latitude": 38.1473472222222,
-          "Longitude": -76.4232111111111
-        },
-        {
-          "Latitude": 38.1461305555556,
-          "Longitude": -76.4266527777778
-        }
-    ],
-  "AirdropBoundary": [
-    {
-      "Latitude": 38.1444444444444,
-      "Longitude": -76.4280916666667
-    },
-    {
-      "Latitude": 38.1459444444444,
-      "Longitude": -76.4237944444445
-    },
-    {
-      "Latitude": 38.1439305555556,
-      "Longitude": -76.4227444444444
-    },
-    {
-      "Latitude": 38.1417138888889,
-      "Longitude": -76.4253805555556
-    },
-    {
-      "Latitude": 38.1412111111111,
-      "Longitude": -76.4322361111111
-    },
-    {
-      "Latitude": 38.1431055555556,
-      "Longitude": -76.4335972222222
-    },
-    {
-      "Latitude": 38.1441805555556,
-      "Longitude": -76.4320111111111
-    },
-    {
-      "Latitude": 38.1452611111111,
-      "Longitude": -76.4289194444444
-    },
-    {
-      "Latitude": 38.1444444444444,
-      "Longitude": -76.4280916666667
-    }
-  ],
-  "Waypoints": [
-    {
-      "Latitude": 38.1446916666667,
-      "Longitude": -76.4279944444445,
-      "Altitude": 200.0
-    },
-    {
-      "Latitude": 38.1461944444444,
-      "Longitude": -76.4237138888889,
-      "Altitude": 300.0
-    },
-    {
-      "Latitude": 38.1438972222222,
-      "Longitude": -76.42255,
-      "Altitude": 400.0
-    },
-    {
-      "Latitude": 38.1417722222222,
-      "Longitude": -76.4251083333333,
-      "Altitude": 400.0
-    },
-    {
-      "Latitude": 38.14535,
-      "Longitude": -76.428675,
-      "Altitude": 300.0
-    },
-    {
-      "Latitude": 38.1508972222222,
-      "Longitude": -76.4292972222222,
-      "Altitude": 300.0
-    },
-    {
-      "Latitude": 38.1514944444444,
-      "Longitude": -76.4313833333333,
-      "Altitude": 300.0
-    },
-    {
-      "Latitude": 38.1505333333333,
-      "Longitude": -76.434175,
-      "Altitude": 300.0
-    },
-    {
-      "Latitude": 38.1479472222222,
-      "Longitude": -76.4316055555556,
-      "Altitude": 200.0
-    },
-    {
-      "Latitude": 38.1443333333333,
-      "Longitude": -76.4322888888889,
-      "Altitude": 200.0
-    },
-    {
-      "Latitude": 38.1433166666667,
-      "Longitude": -76.4337111111111,
-      "Altitude": 300.0
-    },
-    {
-      "Latitude": 38.1410944444444,
-      "Longitude": -76.4321555555556,
-      "Altitude": 400.0
-    },
-    {
-      "Latitude": 38.1415777777778,
-      "Longitude": -76.4252472222222,
-      "Altitude": 400.0
-    },
-    {
-      "Latitude": 38.1446083333333,
-      "Longitude": -76.4282527777778,
-      "Altitude": 200.0
-    }
-  ],
-  "AirdropBoundary": [
-    { "Latitude": 0.0, "Longitude": 0.0, "Altitude": 0.0},
-    { "Latitude": 0.0, "Longitude": 0.0, "Altitude": 0.0},
-    { "Latitude": 0.0, "Longitude": 0.0, "Altitude": 0.0},
-  ],
-  "MappingBoundary": [
-    { "Latitude": 0.0, "Longitude": 0.0, "Altitude": 0.0},
-    { "Latitude": 0.0, "Longitude": 0.0, "Altitude": 0.0},
-    { "Latitude": 0.0, "Longitude": 0.0, "Altitude": 0.0},
-  ],
-})";
-
 /*
  * FILE OUTPUT LOCATIONS
  *  |- build
@@ -218,6 +45,18 @@ int main() {
     std::ofstream file;
 
     LOG_F(WARNING, "RRT Waypoint Testing");
+
+    // Read mission data from JSON file
+    std::ifstream mission_file("../tests/integration/util/mission_data_2020.json");
+    if (!mission_file.is_open()) {
+        LOG_F(ERROR, "Failed to open mission_data_2020.json");
+        return 1;
+    }
+    
+    std::stringstream buffer;
+    buffer << mission_file.rdbuf();
+    std::string mission_json_2020 = buffer.str();
+    mission_file.close();
 
     // Upload the Mission
     DECLARE_HANDLER_PARAMS(state, req, resp);
