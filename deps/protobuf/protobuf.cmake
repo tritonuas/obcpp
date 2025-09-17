df4f7bfc6ec6e292edc8af031d7fd37113c5c960
AskewParity <chrisdenglee@gmail.com>
2025-09-16 22:15:42 -0400

    chore: ensure that we use our version of protoc instead of another library's

--- deps/protobuf/protobuf.cmake
+++ deps/protobuf/protobuf.cmake
@@ -1,5 +1,7 @@
 function(target_add_protobuf target_name)
     include(FindProtobuf)
+    set(PROTOBUF_LIBRARY /usr/lib/x86_64-linux-gnu/libprotobuf.so CACHE FILEPATH "System protobuf lib")
+    set(Protobuf_LIBRARIES ${PROTOBUF_LIBRARY})
     find_package(Protobuf REQUIRED)
 
     target_include_directories(${target_name} PRIVATE ${PROTOBUF_INCLUDE_DIR})
