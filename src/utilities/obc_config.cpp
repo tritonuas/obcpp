c55c7ab2d1190d99a912fb9ad16dc17c89781d93
CSTBook <neilnchaudhury@gmail.com>
2025-10-02 08:50:31 +0000

    removed not-stolen arguments from cv config

--- src/utilities/obc_config.cpp
+++ src/utilities/obc_config.cpp
@@ -78,8 +78,6 @@ OBCConfig::OBCConfig(int argc, char* argv[]) {
     SET_CONFIG_OPT(pathing, coverage, forward, one_way);
 
     SET_CONFIG_OPT(cv, yolo_model_dir);
-    SET_CONFIG_OPT(cv, not_stolen_addr);
-    SET_CONFIG_OPT(cv, not_stolen_port);
 
     SET_CONFIG_OPT_VARIANT(AirdropDropMethod, pathing, approach, drop_method);
     SET_CONFIG_OPT(pathing, approach, drop_angle_rad);
