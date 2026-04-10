8fae1f28805449a39baca62dce6367957adbfe99
Christopher Lee <chrisdenglee@gmail.com>
2026-04-09 17:19:48 -0700

    fix: decrement lap count on last lap

--- src/ticks/fly_waypoints.cpp
+++ src/ticks/fly_waypoints.cpp
@@ -108,6 +108,7 @@ Tick* FlyWaypointsTick::tick() {
             state->getInitPath(), false);
     }
 
+    state->decrementLapsRemaining(); // decrement to zero
     return new MavUploadTick(
         this->state, new FlySearchTick(this->state),
         state->getCoveragePath(), false);
